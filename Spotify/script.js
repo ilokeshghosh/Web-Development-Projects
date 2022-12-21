9edd93126702474bd3caeaf08e7280e34c709e53
Lokesh Ghosh <lghosh857@gmail.com>
2022-12-21 10:55:34 +0530

    .

---
+++ Spotify/script.js
@@ -0,0 +1,14 @@
+console.log("Welcome to Spotify");
+let songs=[
+    {songNmae:"Salam-e-Ishq",filePath:"songs/1.mp3",coverPath:"covers/1.jpg"},
+    {songNmae:"Salam-e-Ishq",filePath:"songs/2.mp3",coverPath:"covers/2.jpg"},
+    {songNmae:"Salam-e-Ishq",filePath:"songs/3.mp3",coverPath:"covers/3.jpg"},
+    {songNmae:"Salam-e-Ishq",filePath:"songs/4.mp3",coverPath:"covers/4.jpg"},
+    {songNmae:"Salam-e-Ishq",filePath:"songs/5.mp3",coverPath:"covers/5.jpg"},
+    {songNmae:"Salam-e-Ishq",filePath:"songs/6.mp3",coverPath:"covers/6.jpg"},
+    {songNmae:"Salam-e-Ishq",filePath:"songs/7.mp3",coverPath:"covers/7.jpg"},
+    {songNmae:"Salam-e-Ishq",filePath:"songs/8.mp3",coverPath:"covers/8.jpg"},
+    {songNmae:"Salam-e-Ishq",filePath:"songs/9.mp3",coverPath:"covers/9.jpg"},
+    {songNmae:"Salam-e-Ishq",filePath:"songs/10.mp3",coverPath:"covers/10.jpg"},
+]
+let audioElement = new Audio("songs/1.mp3");
