c5a1ac3089889d612d146a7d0dad44fd0a8c515b
Lokesh Ghosh <lghosh857@gmail.com>
2023-09-05 23:43:50 +0530

    update

---
+++ travel-agency-landing-page/dist/script.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('#hamburger')
+const drop_menu = document.querySelector('#drop-menu');
+const nav = document.querySelector('#nav')
+
+console.log(hamburger.parentElement);
+// hamburger.addEventListener('click',()=>{
+//     nav.style.display = 'none';
+//     drop_menu.style.display = 'block';
+// }, false)
+
+// drop_menu.addEventListener('click',()=>{
+//     nav.style.display = 'flex';
+//     drop_menu.style.display = 'none';
+//     drop_menu.style.position = 'sticky';
+// }, false)
